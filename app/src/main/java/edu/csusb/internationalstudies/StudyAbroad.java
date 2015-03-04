0526174f9cd9907f090245ab3b8a894e8858cce3
Claudia Tristan <003402242@coyote.csusb.edu>
2015-03-04 12:34:10 -0800

    Adding Drawer Layout to most Activities except a few Other Resources

--- app/src/main/java/edu/csusb/internationalstudies/StudyAbroad.java
+++ app/src/main/java/edu/csusb/internationalstudies/StudyAbroad.java
@@ -3,22 +3,108 @@ package edu.csusb.internationalstudies;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class StudyAbroad extends ActionBarActivity{
+
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_abroad);
+
+        mDrawerList = (ListView)findViewById(R.id.navList);mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        addDrawerItems();
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
+    private void addDrawerItems() {
+        String[] osArray = {"  About Us", "  Advising", "  Admissions", "  Study Abroad", "  Exchange Program", "  Other Resources"};
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(StudyAbroad.this, aboutCisp.class);
+                    startActivity(intent);
+                } else if (position == 1){
+                    Intent intent = new Intent(StudyAbroad.this, secondAdvise.class);
+                    startActivity(intent);
+                }else if (position == 2) {
+                    Intent intent = new Intent(StudyAbroad.this, admissionsInfo.class);
+                    startActivity(intent);
+                }else if (position == 3) {
+                    Intent intent = new Intent(StudyAbroad.this, StudyAbroad.class);
+                    startActivity(intent);
+                }else if (position == 4) {
+                    Intent intent = new Intent(StudyAbroad.this, exchangeProgram.class);
+                    startActivity(intent);
+                }else if (position == 5){
+                    Intent intent = new Intent(StudyAbroad.this, otherResources.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Main");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,6 +122,10 @@ public class StudyAbroad extends ActionBarActivity{
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
